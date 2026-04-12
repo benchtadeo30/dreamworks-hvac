031d2d68e3fdb34bb540d9137c8536abe69b4e82
benchtadeo30 <benchrafael2@gmail.com>
2026-04-13 00:00:41 +0800

    remove env and dist from tracking

--- error.ts
+++ error.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
+import { AppError } from "./utils/AppError";
 
- const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+ const errorHandler = (err: AppError, req: Request, res: Response, next: NextFunction) => {
     if(err.status){
        return res.status(err.status).json({ error: err.message })
     }
