f37ce8136f91836e9619c8f21458d03164f9a02e
benchtadeo30 <benchrafael2@gmail.com>
2026-04-13 00:29:59 +0800

    update gitignore

--- controllers/customerController.ts
+++ controllers/customerController.ts
@@ -1,48 +1,53 @@
-import { NextFunction, Request, Response } from "express"
-import validator from 'validator'
+import { NextFunction, Request, Response } from "express";
+import validator from "validator";
 import { customerDetails } from "../routes/customer";
-import { findCustomerByEmail, matchCustomerPassword } from "../Services/CustomerService";
+import {
+  findCustomerByEmail,
+  matchCustomerPassword,
+} from "../Services/CustomerService";
+import { AppError } from "../utils/AppError";
 
-export const customerLogin = async (req: Request<{}, {}, customerDetails>, res: Response, next: NextFunction) => {
-    const {
-        email,
-        password
-    } = req.body
+export const customerLogin = async (
+  req: Request<{}, {}, customerDetails>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email, password } = req.body;
 
-    if(!validator.isEmail(email)){
-    const err = new Error("Please enter a valid email.")
-     err.status = 400
-     return next(err)
+  if (!validator.isEmail(email)) {
+    return next(new AppError("Please enter a valid email.", 400));
+  }
+
+  try {
+    const findEmail = await findCustomerByEmail(email);
+
+    if (!findEmail.length || !findEmail[0]) {
+      return next(new AppError("Email does not exist!", 400));
     }
 
     try {
-        const findEmail = await findCustomerByEmail(email)
-        
-        if(findEmail.length > 0 && findEmail[0]){
-           try {
-             const verify_account = await matchCustomerPassword(password, findEmail[0].password)
-             if(verify_account){
-            return res.status(200).json({msg: 'Login Successfully!'})
-             }
-             return res.status(400).json({error: 'Password does not match! Please try again.'})
-           } catch (error) {
-            if(error instanceof Error){
-                 const err = new Error(error.message)
-     err.status = 400
-     return next(err)
-            }
-           }
-        }
+      const isValid = await matchCustomerPassword(
+        password,
+        findEmail[0].password
+      );
 
-    const err = new Error("Email does not exist!")
-     err.status = 400
-     return next(err)
+      if (!isValid) {
+        return next(
+          new AppError("Password does not match! Please try again.", 400)
+        );
+      }
 
+      return res.status(200).json({ msg: "Login Successfully!" });
     } catch (error) {
-       if(error instanceof Error){
-         const err = new Error(error.message)
-     err.status = 400
-     return next(err)
-       }
+      if (error instanceof Error) {
+        return next(new AppError(error.message, 400));
+      }
+      return next(new AppError("Something went wrong.", 500));
     }
-}
+  } catch (error) {
+    if (error instanceof Error) {
+      return next(new AppError(error.message, 400));
+    }
+    return next(new AppError("Something went wrong.", 500));
+  }
+};
